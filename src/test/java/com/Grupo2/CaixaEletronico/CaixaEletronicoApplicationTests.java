b3fd58134a0f3ba572ab142c67d235f9ef5cc408
julio.barros <julio.barros@mjv.com.br>
2022-10-06 21:50:27 -0300

    primeiro commit

---
+++ src/test/java/com/Grupo2/CaixaEletronico/CaixaEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo2.CaixaEletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
